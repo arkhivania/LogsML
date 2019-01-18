70c6e55d8815e768bbbf7d0d9e0a533f2b3c1d58
arkhivania <arkhivania@gmail.com>
2019-01-18 12:32:45 +0300

    trainset remove

--- MLRoots/Deduplication/OneItemBag.cs
+++ MLRoots/Deduplication/OneItemBag.cs
@@ -9,15 +9,12 @@ namespace MLRoots.Deduplication
 {
     class OneItemBag
     {
-        const int TrainCount = 25;
         const int MessagesInBag = 1000;
 
         public int CompleteCount => ZipStores.Select(q => q.Lines.Count).Sum();
         public List<ZipStore> ZipStores { get; } = new List<ZipStore>();
 
         public IEnumerable<string> CompleteSet => ZipStores.SelectMany(q => q.Lines);
-
-        public List<string> TrainSet { get; } = new List<string>();
 
         public string BaseMessage { get; }
         public uint Label { get; }
@@ -29,20 +26,16 @@ namespace MLRoots.Deduplication
             this.BaseMessage = baseMessage;
             this.Label = label;            
 
-            AddMessage(baseMessage, true);
+            AddMessage(baseMessage);
         }
 
-        public void AddMessage(string message, bool forceAddToTrain = false)
+        public void AddMessage(string message)
         {
             if (ZipStores.Count == 0
                 || ZipStores[ZipStores.Count - 1].Lines.Count >= MessagesInBag)
                 ZipStores.Add(new ZipStore());
 
             ZipStores[ZipStores.Count - 1].Store(message);
-
-            if (forceAddToTrain || TrainSet.Count < TrainCount)
-                if (forceAddToTrain || random.Next(5) == 0)
-                    TrainSet.Add(message);
         }
     }
 }
